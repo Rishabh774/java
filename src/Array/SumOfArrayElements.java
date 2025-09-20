7b76034cd774c1d7c367acbb6649affcc2c6f280
saurabh <sahu2408saurabh@gmail.com>
2025-09-20 12:14:56 +0530

    New Update

---
+++ src/Array/SumOfArrayElements.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.Scanner;
+
+public class SumOfArrayElements {
+    static int sum(int arr[]){
+        int sum=0;
+        for (int i = 0; i <arr.length ; i++) {
+            sum+=arr[i];
+        }
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array :");
+        int n = sc.nextInt();
+
+        int arr[]= new int[n];
+        System.out.println("Enter "+n+" elements of array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= sc.nextInt();
+        }
+
+        System.out.println("Sum of array elements is "+sum(arr));
+    }
+}

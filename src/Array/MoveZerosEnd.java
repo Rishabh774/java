3e1abf0c1516ef2c0db36e99ef5c6025aa9b41c6
Rishabh774 <sahu2408saurabh@gmail.com>
2025-09-21 01:44:30 +0530

    New Update

---
+++ src/Array/MoveZerosEnd.java
@@ -0,0 +1,37 @@
+package Array;
+
+import java.util.Scanner;
+
+public class MoveZerosEnd {
+    static void movezeros(int arr[]){
+        int count=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]!=0){
+                arr[count++]=arr[i];
+            }
+        }
+        while(count<arr.length){
+            arr[count++]=0;
+        }
+
+    }
+    static void printarr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array :");
+        int n = sc.nextInt();
+
+        int arr[]= new int[n];
+        System.out.print("Enter "+n+" elements of array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= sc.nextInt();
+        }
+        movezeros(arr);
+      printarr(arr);
+    }
+}
---
+++ src/Array/ReverseArray.java
@@ -0,0 +1,42 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+    static void printarr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    static void reverse(int arr[]){
+        int i=0;
+        int j =arr.length-1;
+        while (i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array :");
+        int n = sc.nextInt();
+
+        int arr[]= new int[n];
+        System.out.println("Enter "+n+" elements of array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= sc.nextInt();
+        }
+        System.out.print("Original Array: ");
+        printarr(arr);
+
+        reverse(arr);
+
+        System.out.print("Reversed Array: ");
+        printarr(arr);
+
+    }
+}
